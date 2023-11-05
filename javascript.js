84b39d0c8bf9e50ab6971778eaaf021d66853c12
nathvirginia <148851437+nathvirginia@users.noreply.github.com>
2023-11-04 22:28:43 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,9 +1,30 @@
-function searchCity (event) {
+function updateWeather(response){
+    console.log(response.data.temperature.current);
+    let maintemperature = document.querySelector("#current-temperature");
+    let maintemperatureresponse = response.data.temperature.current;
+    let cityElement = document.querySelector("#current-city");
+    cityElement.innerHTML = response.data.city;
+    
+    maintemperature.innerHTML= Math.round(response.data.temperature.current);
+}
+
+
+
+
+function searchCity (city) {
+    let apiKey = "48o09f8bd70526135tc75884bbfc65a3";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(updateWeather);
+}
+
+
+
+function submitCity (event) {
 event.preventDefault();
 let searchInput = document.querySelector("#search-input");
-let cityElement = document.querySelector("#current-city");
-cityElement.innerHTML = searchInput.value;
+
+searchCity(searchInput.value);
 }
 
 let searchElement = document.querySelector("#search-form");
-searchElement.addEventListener("submit", searchCity);
+searchElement.addEventListener("submit", submitCity);
