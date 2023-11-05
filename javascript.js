a2b92529aaa0b74135b8f9033101b4db7ae48cf9
nathvirginia <148851437+nathvirginia@users.noreply.github.com>
2023-11-05 01:44:05 -0500

    Added Weather Data

--- javascript.js
+++ javascript.js
@@ -3,9 +3,30 @@ function updateWeather(response){
     let maintemperature = document.querySelector("#current-temperature");
     let maintemperatureresponse = response.data.temperature.current;
     let cityElement = document.querySelector("#current-city");
-    cityElement.innerHTML = response.data.city;
+    let maindescriptionresponse = response.data.condition.description;
+    let descriptionElement = document.querySelector("#current-description");
+    let mainhumidityresponse = response.data.temperature.humidity;
+    let humidityElement = document.querySelector("#current-humidity");
+    let mainwindresponse = response.data.wind.speed;
+    let windElement = document.querySelector("#current-wind");
+    let currentDate = new Date();
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let currentDay = days[currentDate.getDay()]; 
+    let currentMonth = months[currentDate.getMonth()]; 
+    let currentDayElement = document.querySelector("#current-day"); 
+    let mainDateElement = document.querySelector("#main-date"); 
 
+    mainDateElement.innerHTML = `${currentDay}, ${currentMonth} ${currentDate.getDate()}`;
+    currentDayElement.innerHTML = currentDate.getDate();
+    cityElement.innerHTML = response.data.city;
     maintemperature.innerHTML= Math.round(response.data.temperature.current);
+    descriptionElement.innerHTML = maindescriptionresponse;
+    humidityElement.innerHTML = mainhumidityresponse;
+    windElement.innerHTML = mainwindresponse;
+
+
+
 }
 
 
